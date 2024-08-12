0811e0d1fd9f920385adcf475a6cedd1b3dda52b
Emmanuel Worgu <tech@visibuy.com.ng>
2024-08-12 16:48:35 -0400

    started working on the project

---
+++ src/StatefulComponent/WaitListComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import WaitList from "../StatelessComponent/waitList";
+
+function WaitListComp () {
+    return (
+        <WaitList />
+    );
+};
+
+export default WaitListComp;
---
+++ src/StatelessComponent/waitList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../StyleSheets/waitList.css'
+
+function WaitList () {
+    return (
+        <div>
+            <div className='header-container'>
+                <div className='logo'>Logo</div>
+                <div className='menu'>Menu</div>
+            </div>
+        </div>
+    );
+};
+
+export default WaitList;
