603ee8b326dd77cca8d260f71aed08fcb5d471de
Emmanuel Worgu <tech@visibuy.com.ng>
2024-09-17 23:25:26 -0400

    updated react syntax

--- src/ui/Customers.jsx
+++ src/ui/Customers.jsx
@@ -2,7 +2,7 @@ function Customers() {
     return (
         <div>
             <div className='iframe-div'>
-                <iframe width="560" height="315" src="https://youtu.be/yyQOVRFtLnU" frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://youtu.be/yyQOVRFtLnU" frameBorder="0" allowFullScreen></iframe>
             </div>
             <div className="why-visibuy">
                 {/* Don't Know how to arrange the benefits of visibuy. Check in the document. I guess youtube is not showing because its localhost */}
