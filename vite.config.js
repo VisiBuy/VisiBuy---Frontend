4b538e862d13342bc7466fb5490eac0d19b82e4c
Emmanuel Worgu <tech@visibuy.com.ng>
2024-09-21 23:56:24 -0400

    fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 export default defineConfig({
   plugins: [
     react(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,  // Ensure Dart Sass is used
+        api: 'modern', // Use modern API or 'modern-compiler'
+      },
+    },
+  },
 });
