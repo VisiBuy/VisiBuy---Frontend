9b5d9797831a624685d39a9ecc44c476a20fab3d
Emmanuel Worgu <tech@visibuy.com.ng>
2024-09-22 06:39:16 -0400

    fixed github 404 error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  base: "/VisiBuy---Frontend",
+  // base: "/VisiBuy---Frontend",
   css: {
     preprocessorOptions: {
       scss: {
