d79a9aed2ef9f834ad8274fdc937f421ec40f37d
Emmanuel Worgu <tech@visibuy.com.ng>
2024-09-21 22:37:34 -0400

    fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ViteToml } from 'vite-plugin-toml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteToml()],
-  build: {
-    rollupOptions: {
-      external: ['wrangler.toml'],
-    },
-  },
+  plugins: [react()],
 });
