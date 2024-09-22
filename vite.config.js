d872d15433791d92a9f8376c9888640ea1fa9755
Emmanuel Worgu <tech@visibuy.com.ng>
2024-09-21 23:11:07 -0400

    fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sass from 'sass';
+import sass from 'vite-plugin-sass';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  ],
-  css: {
-    preprocessorOptions: {
-      sass: {
-        implementation: sass,
-        // Silencing deprecation warnings
+    sass({
+      sassOptions: {
         silenceDeprecations: ['legacy-js-api'],
       },
-    },
-  },
+    }),
+  ],
 });
