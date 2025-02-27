e41cf15f3cdc2589aca7b83c7ee7687f3af318dc
SOPMAN5 <SOPMAN5@users.noreply.github.com>
2025-02-27 21:41:42 +0000

    deploy: 0e36746968b4efae0cd95fd454600413be5cea07

---
+++ assets/circle-check-big-B70_xvxD.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-Cfnk0UlR.js";/**
+ * @license lucide-react v0.455.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("CircleCheckBig",[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);export{t as default};
