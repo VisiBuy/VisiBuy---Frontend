69e7dfc8bba06b4b702c728d3f6be9e3b9efd47c
Chiamaka-Uchenna <Chiamaka-Uchenna@users.noreply.github.com>
2025-02-27 09:29:38 +0000

    deploy: f7e05112084614b13a8b1e5758f91d32cb23ad21

---
+++ assets/chevron-left-CjKw1sef.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-BISVAS3E.js";/**
+ * @license lucide-react v0.455.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{o as default};
