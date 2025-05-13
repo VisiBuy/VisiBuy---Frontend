3e97144f87ba09d9e27b9ef19aacd687f5d75b95
Codrex <ojodare73@gmail.com>
2025-05-13 21:30:53 +0100

    fixed all errors

--- src/modules/Buyer/features/checkout/Checkout.tsx
+++ src/modules/Buyer/features/checkout/Checkout.tsx
@@ -130,7 +130,6 @@ const Checkout = () => {
       console.log("Payment modal closed"); // Add this to handle modal close event
     },
   };
-  console.log(flutterwaveConfig.public_key);
 
   return (
     <div className="p-6 fixed inset-0 bg-black bg-opacity-10 flex flex-col justify-center items-center">
