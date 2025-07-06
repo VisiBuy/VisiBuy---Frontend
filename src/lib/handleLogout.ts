4809b0e2dcf8be7d3674d89c2ce1beaf7c83fe88
David Ahukanna <ahukannadavid5@gmail.com>
2025-07-06 18:28:59 +0100

    added address to footer and fixed return path issues

--- src/lib/handleLogout.ts
+++ src/lib/handleLogout.ts
@@ -2,8 +2,9 @@ import { useAppDispatch } from "@/hooks/app-hooks";
 import { logout } from "@/modules/Auth/features/slices";
 import { store } from "@/store/store";
 export const handleLogout = () => {
+  localStorage.clear();
   store.dispatch(logout());
-
+  
   // Redirect to login page
-  window.location.href = "/#/login";
+  window.location.href = "/login";
 };
